db9c392681b99bfd21bd4602d917238d2a1c2364
Zanges <3868054+Zanges@users.noreply.github.com>
2025-06-07 23:30:05 +0200

    Rename project to Series Renamer and update related files

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,9 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "Series Renamer",
         native_options,
-        Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(series_renamer::TemplateApp::new(cc)))),
     )
 }
 
@@ -50,7 +50,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(series_renamer::TemplateApp::new(cc)))),
             )
             .await;
 
