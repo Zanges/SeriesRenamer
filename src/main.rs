9b7a86e3a50ee05d2df930edc27560712e05c903
Zanges <3868054+Zanges@users.noreply.github.com>
2025-06-07 23:35:42 +0200

    Rename TemplateApp to SeriesRenamer in app, lib, and main files

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "Series Renamer",
         native_options,
-        Box::new(|cc| Ok(Box::new(series_renamer::TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(series_renamer::SeriesRenamer::new(cc)))),
     )
 }
 
@@ -50,7 +50,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(series_renamer::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(series_renamer::SeriesRenamer::new(cc)))),
             )
             .await;
 
